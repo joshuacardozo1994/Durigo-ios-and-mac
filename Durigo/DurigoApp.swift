9800909d8443d51b27b649d6b0b21c275b408720
joshuacardozo1994 <joshua.cardozo@gmail.com>
2023-10-27 00:08:40 +0530

    Initial Commit

---
+++ Durigo/DurigoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DurigoApp.swift
+//  Durigo
+//
+//  Created by Joshua Cardozo on 27/10/23.
+//
+
+import SwiftUI
+
+@main
+struct DurigoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

cb49307ff10908b22fca134a9d1cae3f8a0dded1
joshuacardozo1994 <joshua.cardozo@gmail.com>
2023-12-20 18:56:13 +0530

    added usage description for faceID

--- Durigo/Billing/BillHistoryList.swift
+++ Durigo/Billing/BillHistoryList.swift
@@ -237,7 +237,9 @@ struct BillHistoryList: View {
             }
         }
         .task {
-            authenticateWithBiometrics()
+            if !isUnlocked {
+                authenticateWithBiometrics()
+            }
         }
     }
 }

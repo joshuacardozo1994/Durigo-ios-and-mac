5c809e4a4aae24805ebe79543527a96e44118829
joshuacardozo1994 <joshua.cardozo@gmail.com>
2023-12-30 19:00:57 +0530

    added today section for stats

--- Durigo/Shared/Extensions/Date+Extensions.swift
+++ Durigo/Shared/Extensions/Date+Extensions.swift
@@ -18,3 +18,41 @@ extension Date {
         return formatter.string(from: self)
     }
 }
+
+extension Date {
+    
+    private func isTimeBetween12AMAnd6AM(date: Date) -> Bool {
+        // Get the current calendar
+        let calendar = Calendar.current
+        
+        // Extract the hour component from the date
+        let hour = calendar.component(.hour, from: date)
+        
+        // Check if the hour is between 0 and 3 (0 is 12 AM, and 3 is 3 AM)
+        return hour >= 0 && hour < 6
+    }
+    
+    private func isDateBetween11AMAndNow(date: Date, yesterday: Bool) -> Bool {
+        let calendar = Calendar.current
+
+        // Get the current date and time
+        let now = Date()
+        
+        // Calculate day's date
+        guard let day = calendar.date(byAdding: .day, value: yesterday ? -1 : 0, to: now) else { return false }
+        
+        // Set day's date to 11 AM
+        var components = calendar.dateComponents([.year, .month, .day], from: day)
+        components.hour = 11
+        components.minute = 0
+        components.second = 0
+        guard let day11AM = calendar.date(from: components) else { return false }
+
+        // Check if the given date is between yesterday at 11 AM and now
+        return date >= day11AM && date <= now
+    }
+    
+    func isBetweenOperatingHoursToday() -> Bool {
+        return isDateBetween11AMAndNow(date: self, yesterday: isTimeBetween12AMAnd6AM(date: self))
+    }
+}
